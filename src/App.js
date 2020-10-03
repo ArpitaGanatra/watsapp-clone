2d87481f1635922f7eae036b47726d720a6660ff
ArpitaGanatra <ganatraarpita@gmail.com>
2020-10-03 10:55:45 +0530

    adding backend integration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import {BrowserRouter as Router } from 'react-router-dom';
+import React,{useEffect,useState} from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
-
+import Login from './Login'
+import { useStateValue } from './StateProvider';
 
 function App() {
+
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="App">
-      <div className='app__body'> 
+
+      {!user ? (
+        <Login />
+      ):(
+        <div className='app__body'> 
+      <Router>
+      <Switch>
+
+      <Route path='/rooms/:roomId'>
         <Sidebar/>
         <Chat/>
+      </Route>  
+
+        <Route path='/'>
+        <Sidebar/>
+        </Route>
+
+      </Switch>
+      </Router>
       </div>
+      ) }
+
+      
     </div>
   );
 }
